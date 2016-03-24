37b676c2fd0f23daf66b89623922065f67787ab9
Ariane Deschenes Foucher <ariane.df@me.com>
2016-03-24 14:31:24 -0400

    Created a class Person that initializes the name

--- people.rb
+++ people.rb
@@ -1,12 +1,20 @@
+# STEP 4
+class Person
+  def intialize(name)
+    @name = name
+  end
+end
 # STEP 1
 class Student
 # STEP 2
   def learn
     "I get it!"
+  end
 end
 
 class Instructor
 # STEP 3
   def teach
     "Everything in Ruby is an Object."
+  end
 end
