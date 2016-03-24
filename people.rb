602e935d14357fb3eb92cba1d4f1a1c857369d53
Ariane Deschenes Foucher <ariane.df@me.com>
2016-03-24 15:01:54 -0400

    Created an Instructor named Chris and greeted him

--- people.rb
+++ people.rb
@@ -1,25 +1,31 @@
 # STEP 4
 class Person
-  def intialize(name)
+  attr_accessor = :name
+
+  def initialize(name)
     @name = name
   end
 
 #STEP 5
   def greeting
-    "Hi, my name is #{name}."
+    "Hi, my name is #{@name}."
   end
 end
 # STEP 1
-class Student
+class Student < Person
 # STEP 2
   def learn
     "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
 # STEP 3
   def teach
     "Everything in Ruby is an Object."
   end
 end
+
+# STEP 6
+chris = Instructor.new("Chris")
+chris.greeting
