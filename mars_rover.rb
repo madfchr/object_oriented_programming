0cc8e1d7623edffd016f71782cc77af92463b133
Ariane Deschenes Foucher <ariane.df@me.com>
2016-03-25 15:02:24 -0400

    Added input.

--- mars_rover.rb
+++ mars_rover.rb
@@ -7,19 +7,20 @@ class Rover
     @direction = direction
   end
 
-  def read_instruction(instruction)
-    instruction.map do |value|
+  def read_instructions(instructions)
+    instructions.map do |value|
       if value == "L" || value == "R"
         self.turn(value)
       elsif value == "M"
         self.move
       else
-        puts "Invalid instruction!"
+        puts "Invalid instructions!"
       end
     end
   end
 
   def move
+    # Affects the Rover's position
       if @direction == "E"
         @x_coordinate += 1
       elsif @direction == "W"
@@ -34,6 +35,7 @@ class Rover
   end
 
   def turn(turning)
+    # Affects the Rover's direction
     if (turning == "L" && @direction == "N") || (turning == "R" && @direction == "S")
       @direction = "W"
     elsif (turning == "R" && @direction == "N") || (turning == "L" && @direction == "S")
@@ -44,7 +46,24 @@ class Rover
       @direction = "S"
     else
       puts "What do you mean?"
+      # When you don't want me to move, but you tell me to go
+      # What do you mean?
     end
   end
 
 end
+
+# INPUTS (5)
+puts "What's the size of the plateau?"
+plateau = gets.chomp
+puts "What's the Rover's current position? Enter x coordinate:"
+x_start = gets.chomp.to_i
+puts "Thanks. Now enter the y coordinate:"
+y_start = gets.chomp.to_i
+puts "What's the Rovers's direction? Enter N, E, S or W."
+direction = gets.chomp
+puts "All set. Now how should the Rover move accros the plateau? Enter R to turn right, L to turn left or M to move forward."
+instructions = gets.chomp
+array_instructions = instructions.split(" ")
+
+rover_1 = Rover.new(x_start, y_start, direction)
