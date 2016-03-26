957960fb36668b080a5b08c29129a2f346afffd0
Ariane Deschenes Foucher <ariane.df@me.com>
2016-03-26 16:08:05 -0400

    Added input

--- sales_tax.rb
+++ sales_tax.rb
@@ -9,4 +9,18 @@ class Sales_taxes
 
   def import_duty
   end
-  
+end
+
+  # INPUTS
+  puts "Would you like to add an item to the list?"
+  answer = gets.chomp.downcase
+  if answer == "yes"
+    puts "Enter item:"
+    item = gets.chomp
+    puts "Enter price:"
+    price = gets.chomp
+  else
+    puts "Okay, here's your receipt."
+    puts # RECEIPT HASH
+
+  end
