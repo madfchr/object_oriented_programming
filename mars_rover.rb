d2de715843341b9a03a38d611977cb44162488e8
Ariane Deschenes Foucher <ariane.df@me.com>
2016-03-25 22:00:05 -0400

    Mars Rover completed. Will come back to work on the optional part.

--- mars_rover.rb
+++ mars_rover.rb
@@ -8,41 +8,41 @@ class Rover
   end
 
   def read_instructions(instructions)
-    instructions.map do |value|
-      if value == "L" || value == "R"
-        self.turn(value)
-      elsif value == "M"
+    instructions.map do |instruction|
+      if instruction == "L" || instruction == "R"
+        self.turn(instruction)
+      elsif instruction == "M"
         self.move
       else
-        puts "Invalid instructions!"
+        puts "Invalid instruction!"
       end
     end
   end
 
   def move
     # Affects the Rover's position
-      if @direction == "E"
-        @x_coordinate += 1
-      elsif @direction == "W"
-        @x_coordinate -= 1
-      elsif @direction == "N"
+      if @direction == "N"
         @y_coordinate += 1
+      elsif @direction == "E"
+        @x_coordinate += 1
       elsif @direction == "S"
         @y_coordinate -= 1
+      elsif @direction == "W"
+        @x_coordinate -= 1
       else
         puts "Invalid direction!"
       end
   end
 
-  def turn(turning)
+  def turn(turn_command)
     # Affects the Rover's direction
-    if (turning == "L" && @direction == "N") || (turning == "R" && @direction == "S")
+    if (turn_command == "L" && @direction == "N") || (turn_command == "R" && @direction == "S")
       @direction = "W"
-    elsif (turning == "R" && @direction == "N") || (turning == "L" && @direction == "S")
+    elsif (turn_command == "R" && @direction == "N") || (turn_command == "L" && @direction == "S")
       @direction = "E"
-    elsif (turning == "L" && @direction == "E") || (turning == "R" && @direction == "W")
+    elsif (turn_command == "L" && @direction == "E") || (turn_command == "R" && @direction == "W")
       @direction = "N"
-    elsif (turning == "R" && @direction == "E") || (turning == "L" && @direction == "W")
+    elsif (turn_command == "R" && @direction == "E") || (turn_command == "L" && @direction == "W")
       @direction = "S"
     else
       puts "What do you mean?"
@@ -51,19 +51,26 @@ class Rover
     end
   end
 
+  def output
+    puts "Rover's position is #{@x_coordinate}, #{@y_coordinate}, #{@direction}"
+  end
+
 end
 
 # INPUTS (5)
-puts "What's the size of the plateau?"
-plateau = gets.chomp
-puts "What's the Rover's current position? Enter x coordinate:"
-x_start = gets.chomp.to_i
+# puts "What's the size of the plateau?"
+# plateau = gets.chomp
+# I don't need this for now.
+puts "What's the Rover's initial position? Enter x coordinate:"
+x_initial = gets.chomp
 puts "Thanks. Now enter the y coordinate:"
-y_start = gets.chomp.to_i
+y_initial = gets.chomp
 puts "What's the Rovers's direction? Enter N, E, S or W."
 direction = gets.chomp
 puts "All set. Now how should the Rover move accros the plateau? Enter R to turn right, L to turn left or M to move forward."
 instructions = gets.chomp
-array_instructions = instructions.split(" ")
+array_instructions = instructions.split(//)
 
-rover_1 = Rover.new(x_start, y_start, direction)
+rover_1 = Rover.new(x_initial.to_i, y_initial.to_i, direction)
+rover_1.read_instructions(array_instructions)
+rover_1.output
